f7e928145571eb8861c811f8a911bbc67e1b3996
ken Robesky <ken@robesky.org>
2023-04-30 05:58:58 -0700

    fix getClients query

--- src/db/clientDB_Impl.js
+++ src/db/clientDB_Impl.js
@@ -658,7 +658,7 @@ async function getClients(publicIPAddress, localSentinelsOnly, userId) {
     } else {
       //if (!userId) {
       selectStatement =
-        "SELECT *, UserName, IspName, PublicIpAddress = " + publicIPAddress + " AS IsLocalClient FROM ClientInstance " +
+        "SELECT *, UserName, IspName, PublicIpAddress = \"" + publicIPAddress + "\" AS IsLocalClient FROM ClientInstance " +
         "LEFT JOIN User ON User.Id = ClientInstance.UserId " +
         "LEFT JOIN InternetServiceProvider ON InternetServiceProvider.AutonomousSystemNumber = ClientInstance.IspAutonomousSystemNumber " +
         "WHERE ClientType = 'appliance' && userId = ClientInstance.UserId " +
